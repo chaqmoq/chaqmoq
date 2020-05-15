85f229463d8ef941828f7b137623df120a451d12
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-15 21:05:46 +0500

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -9,10 +9,11 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/chaqmoq/http.git", .branch("master")),
-        .package(url: "https://github.com/chaqmoq/routing.git", .branch("master"))
+        .package(url: "https://github.com/chaqmoq/routing.git", .branch("master")),
+        .package(url: "https://github.com/chaqmoq/validation.git", .branch("master"))
     ],
     targets: [
-        .target(name: "Chaqmoq", dependencies: ["HTTP", "Routing"]),
+        .target(name: "Chaqmoq", dependencies: ["HTTP", "Routing", "Validation"]),
         .testTarget(name: "ChaqmoqTests", dependencies: ["Chaqmoq"])
     ],
     swiftLanguageVersions: [.v5]

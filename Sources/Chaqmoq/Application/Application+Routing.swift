f5e06008d6738707b72553c93d0660b5a597f46b
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-28 19:30:02 +0500

    Add default path

--- Sources/Chaqmoq/Application/Application+Routing.swift
+++ Sources/Chaqmoq/Application/Application+Routing.swift
@@ -3,44 +3,44 @@ import Routing
 
 extension Application {
     @discardableResult
-    public func delete(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func delete(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.DELETE], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func get(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func get(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.GET], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func head(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func head(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.HEAD], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func options(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func options(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.OPTIONS], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func patch(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func patch(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.PATCH], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func post(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func post(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.POST], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
-    public func put(_ path: String, name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
+    public func put(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
         return request(methods: [.PUT], path: path, name: name, requestHandler: requestHandler).first
     }
 
     @discardableResult
     public func request(
         methods: Set<Request.Method>? = nil,
-        path: String,
+        path: String = "/",
         name: String? = nil,
         requestHandler: @escaping Route.RequestHandler
     ) -> Set<Route> {

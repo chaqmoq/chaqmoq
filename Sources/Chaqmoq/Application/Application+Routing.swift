60069ae11809ae441877bfa78417559af141d859
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-05-28 19:37:30 +0500

    Rename closure

--- Sources/Chaqmoq/Application/Application+Routing.swift
+++ Sources/Chaqmoq/Application/Application+Routing.swift
@@ -3,38 +3,38 @@ import Routing
 
 extension Application {
     @discardableResult
-    public func delete(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.DELETE], path: path, name: name, requestHandler: requestHandler).first
+    public func delete(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.DELETE], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func get(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.GET], path: path, name: name, requestHandler: requestHandler).first
+    public func get(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.GET], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func head(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.HEAD], path: path, name: name, requestHandler: requestHandler).first
+    public func head(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.HEAD], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func options(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.OPTIONS], path: path, name: name, requestHandler: requestHandler).first
+    public func options(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.OPTIONS], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func patch(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.PATCH], path: path, name: name, requestHandler: requestHandler).first
+    public func patch(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.PATCH], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func post(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.POST], path: path, name: name, requestHandler: requestHandler).first
+    public func post(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.POST], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
-    public func put(_ path: String = "/", name: String? = nil, requestHandler: @escaping Route.RequestHandler) -> Route? {
-        return request(methods: [.PUT], path: path, name: name, requestHandler: requestHandler).first
+    public func put(_ path: String = "/", name: String? = nil, handler: @escaping Route.RequestHandler) -> Route? {
+        return request(methods: [.PUT], path: path, name: name, handler: handler).first
     }
 
     @discardableResult
@@ -42,13 +42,13 @@ extension Application {
         methods: Set<Request.Method>? = nil,
         path: String = "/",
         name: String? = nil,
-        requestHandler: @escaping Route.RequestHandler
+        handler: @escaping Route.RequestHandler
     ) -> Set<Route> {
         let methods = methods ?? Set(Request.Method.allCases)
         var routes: Set<Route> = []
 
         for method in methods {
-            if let route = Route(method: method, path: path, name: name, requestHandler: requestHandler) {
+            if let route = Route(method: method, path: path, name: name, requestHandler: handler) {
                 router.routeCollection.insert(route)
                 routes.insert(route)
             }

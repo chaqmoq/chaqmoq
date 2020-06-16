76b04c849d76e3f76627bbfd04a28eab203fdec3
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2020-06-16 20:26:19 +0500

    Refactor

--- Sources/Chaqmoq/Application.swift
+++ Sources/Chaqmoq/Application.swift
@@ -2,19 +2,21 @@ import HTTP
 import Routing
 
 public final class Application {
-    private let server: Server
-    public let router: Router
+    let server: Server
+    let router: Router
 
     public init() {
         self.server = Server()
         self.router = Router()
 
         server.onReceive = { [weak self] request, eventLoop in
-            if let uri = request.uri.string, let route = self?.router.resolveRouteBy(method: request.method, uri: uri) {
-                return route.requestHandler(request)
+            guard
+                let uri = request.uri.string,
+                let route = self?.router.resolveRouteBy(method: request.method, uri: uri) else {
+                    return Response(status: .notFound)
             }
 
-            return Response(status: .notFound)
+            return route.requestHandler(request)
         }
     }
 

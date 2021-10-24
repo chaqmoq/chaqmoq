3a33401eb692b5378a656afc28465e0dad3954a0
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-10-24 06:29:37 +0500

    Use `Request` attributes to set the current `Route`

--- Sources/Chaqmoq/Middleware/RoutingMiddleware.swift
+++ Sources/Chaqmoq/Middleware/RoutingMiddleware.swift
@@ -6,19 +6,13 @@ public struct RoutingMiddleware: Middleware {
     @Injected private var router: Router
 
     public func handle(request: Request, nextHandler: @escaping (Request) -> Response) -> Response {
-        var request = request
-
         if let route = router.resolveRoute(for: request) {
-            request.uri.setParameter("_route", value: route)
-
-            if let parameters = route.parameters {
-                for parameter in parameters {
-                    request.uri.setParameter(parameter.name, value: parameter.value)
-                }
-            }
+            var request = request
+            request.setAttribute("_route", value: route)
 
             let result = route.handler(request)
             if let response = result as? Response { return response }
+
             return Response("\(result)")
         }
 

fa19b9ac169a1e9172486d5120c6749a9cd93bb4
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-02 11:30:15 +0500

    Add documentation for RoutingMiddleware

--- Sources/Chaqmoq/Middleware/RoutingMiddleware.swift
+++ Sources/Chaqmoq/Middleware/RoutingMiddleware.swift
@@ -2,11 +2,17 @@ import HTTP
 import Resolver
 import Routing
 
+/// Resolves a `Route` for a `Request` and calls its handler method. Returns a `.notFound` `Response` if it can't match any `Route`s. By default, the
+/// `RoutingMiddleware` is already registered for `Chaqmoq` applications. If you override the `middleware` property, make sure you add it explicitly.
+/// Keep in mind that it is almost always better to register the `RoutingMiddleware` at the end of `Middleware` stack.
 public struct RoutingMiddleware: Middleware {
+    /// The current application's `Router`.
     @Injected public private(set) var router: Router
 
+    /// Initializes a new instance of `RoutingMiddleware`.
     public init() {}
 
+    /// See `Middleware`.
     public func handle(request: Request, nextHandler: @escaping (Request) -> Response) -> Response {
         if let route = router.resolveRoute(for: request) {
             var request = request

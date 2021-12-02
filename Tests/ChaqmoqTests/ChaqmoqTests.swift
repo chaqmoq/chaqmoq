5156b6c4e37fdf25d6c61e903d0ef55f01ac0cab
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-02 11:33:24 +0500

    Remove the example test

--- Tests/ChaqmoqTests/ChaqmoqTests.swift
+++ Tests/ChaqmoqTests/ChaqmoqTests.swift
@@ -1,21 +1,4 @@
 @testable import Chaqmoq
 import XCTest
 
-final class ChaqmoqTests: XCTestCase {
-    func test1() {
-        let app = Chaqmoq()
-        app.middleware = [
-            HTTPMethodOverrideMiddleware(),
-            CORSMiddleware(),
-            RoutingMiddleware()
-        ]
-
-        app.post("/hello/{name}", middleware: [CORSMiddleware()]) { request in
-            let route: Route = request.getAttribute("_route")!
-            let parameter = route.parameters?.first(where: { $0.name == "name" })
-            return "Hello \(parameter?.value ?? "")"
-        }
-
-        try! app.run()
-    }
-}
+final class ChaqmoqTests: XCTestCase {}

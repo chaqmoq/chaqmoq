9c0604f3f54042a1cded4ae267931099c87a0407
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-12-04 15:21:36 +0500

    Add test

--- Tests/ChaqmoqTests/ChaqmoqTests.swift
+++ Tests/ChaqmoqTests/ChaqmoqTests.swift
@@ -1,4 +1,18 @@
 @testable import Chaqmoq
+import Resolver
 import XCTest
 
-final class ChaqmoqTests: XCTestCase {}
+final class ChaqmoqTests: XCTestCase {
+    func testDefaultInit() {
+        // Act
+        let app = Chaqmoq()
+
+        // Assert
+        XCTAssertEqual(app.configuration, Chaqmoq.Configuration())
+        XCTAssertEqual(app.environment, .development)
+        XCTAssertTrue(app.eventLoopGroup === app.server.eventLoopGroup)
+        XCTAssertEqual(app.middleware.count, 1)
+        XCTAssertTrue(type(of: app.middleware.first!) == RoutingMiddleware.self)
+        XCTAssertTrue(app.resolver === Resolver.main)
+    }
+}

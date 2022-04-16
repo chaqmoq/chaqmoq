32e624bdbd1d93af20ebc7da01669ba2e6aae463
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-17 00:45:56 +0500

    Set the minimum platform versions

--- Package.swift
+++ Package.swift
@@ -4,6 +4,12 @@ import PackageDescription
 
 let package = Package(
     name: "chaqmoq",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v12),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(name: "Chaqmoq", targets: ["Chaqmoq"])
     ],

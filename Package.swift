93835f0dacaf1a8a1f633c4ba113af30a21c965c
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2022-04-16 23:37:42 +0500

    Set the platform versions

--- Package.swift
+++ Package.swift
@@ -4,12 +4,6 @@ import PackageDescription
 
 let package = Package(
     name: "chaqmoq",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v12),
-        .tvOS(.v13),
-        .watchOS(.v6)
-    ],
     products: [
         .library(name: "Chaqmoq", targets: ["Chaqmoq"])
     ],

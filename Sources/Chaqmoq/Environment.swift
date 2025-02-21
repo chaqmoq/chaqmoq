5fe8c55c93b0aaa3fa8e94f6755a84f13c6a77ff
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2025-02-21 15:29:07 +0500

    Fix minor issues

--- Sources/Chaqmoq/Environment.swift
+++ Sources/Chaqmoq/Environment.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-/// Manages an application's `Environment` such as `.production`, `.development`, and `.testing`.Enables to create a
+/// Manages an application's `Environment` such as `.production`, `.development`, and `.testing`. Enables to create a
 /// custom `Environment` and provides access to `Environment` variables.
 public struct Environment {
     /// The name of an `Environment`.
@@ -10,8 +10,8 @@ public struct Environment {
     ///
     /// - Parameter name: The name for a new `Environment`. Defaults to the `Environment` variable with the
     /// `CHAQMOQ_ENV` key or falls back to the `development` `Environment` if it doesn't exist or not provided.
-    public init(name: String = Environment.get("CHAQMOQ_ENV") ?? "") {
-        self.name = name.isEmpty ? Environment.development.name : name
+    public init(name: String = Self.get("CHAQMOQ_ENV") ?? "") {
+        self.name = name.isEmpty ? Self.development.name : name
     }
 
     /// Gets an `Environment` variable by key.
@@ -25,18 +25,18 @@ public struct Environment {
 
 extension Environment {
     /// An `Environment` for deploying an application.
-    public static let production = Environment(name: "production")
+    public static let production = Self(name: "production")
 
     /// An `Environment` for developing an application.
-    public static let development = Environment(name: "development")
+    public static let development = Self(name: "development")
 
     /// An `Environment` for testing an application.
-    public static let testing = Environment(name: "testing")
+    public static let testing = Self(name: "testing")
 }
 
 extension Environment: Equatable {
     /// See `Equatable`.
-    public static func == (lhs: Environment, rhs: Environment) -> Bool {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
         lhs.name == rhs.name
     }
 }
